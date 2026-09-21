8f41bae8b6571103b9d03837b6acf83dd0dbe1b9
b-atteryacid <goadam80@gmail.com>
2026-09-21 16:42:43 -0600

    Create forks per arg

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 uint32_t fib(uint32_t n) {
     uint32_t a = 0;
@@ -36,13 +39,26 @@ int main(int argc, char** argv) {
     if(argc == 1) {
         return -1;
     }
+
+    __pid_t children[8] = {0};
     for(int i = 1; i < argc; i++) {
         const char* thisArg = argv[i];
-        printf("got argument: ");
-        printf(thisArg);
+        printf("got argument: %s\n",thisArg);
         uint32_t thisInt = strToInt(thisArg);
-        printf("\nthat is %d in int!!\n",thisInt);
-        printf("fib %d: %d\n",thisInt,fib(thisInt));
+        children[i-1] = fork();
+        if(children[i-1] < 0) {
+            printf("Could not create fork\n");
+            exit(children[i-1]);
+        } else if (children[i-1] == 0) { //child
+            printf("child calculating %d\n",thisInt);
+            exit(fib(thisInt));
+        }
+    }
+
+    for(int i = 0; i < sizeof(children)/sizeof(children[0]); i++) {
+        int stat = 0;
+        waitpid(children[i],&stat,0);
+
     }
 
     return 0;
