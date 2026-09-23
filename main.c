9f0089dfd809d6b96e20461df24550895296ef08
b-atteryacid <goadam80@gmail.com>
2026-09-22 19:55:35 -0600

    Rename some vars for clarity, add pipe error checking, rework printing locations

--- main.c
+++ main.c
@@ -10,68 +10,81 @@
 uint32_t fib(uint32_t n) {
     uint32_t a = 0;
     uint32_t b = 1;
+    uint32_t temp = 0;
     uint32_t i = 0;
-    uint32_t c = 0;
 
     if(n == 0) {
         return 0;
     }
 
     for(i = 0; i < n-1; i++) {
-        c = a + b;
+        temp = a + b;
         a = b;
-        b = c;
+        b = temp;
     }
 
     return b;
-
 }
 
-uint32_t strToInt(const char* s) {
-    uint32_t r = 0;
-    while(*s != '\0') {
-        r *= 10;
-        r += *s - '0';
-        s++;
+uint32_t strToInt(const char* str) {
+    uint32_t result = 0;
+
+    while(*str != '\0') {
+        if(*str < '0' || *str > '9') {
+            return result;
+        }
+        result *= 10;
+        result += *str - '0';
+        str++;
     }
-    return r;
+
+    return result;
 }
 
 int main(int argc, char** argv) {
 
     if(argc == 1 || argc-1 > MAX_ARGS) {
+        printf("Usage: ./main (x0) [x1] [x2] ... [x%d]\n",MAX_ARGS-1);
         return -1;
     }
 
     __pid_t children[MAX_ARGS] = {0};
-    int pipes[2][MAX_ARGS];
+    int pipes[2][MAX_ARGS] = {0};
+
     for(int i = 1; i < argc; i++) {
         const char* thisArg = argv[i];
         uint32_t thisInt = strToInt(thisArg);
-        pipe(pipes[i-1]);
+
+        if(pipe(pipes[i-1]) < 0) {
+            printf("Could not create pipe\n");
+            exit(-1);
+        }
         children[i-1] = fork();
+
         if(children[i-1] < 0) {
             printf("Could not create fork\n");
             exit(children[i-1]);
+
         } else if (children[i-1] == 0) { //child
             char buf[64];
             sprintf(buf,"%d",fib(thisInt));
             if(write(pipes[i-1][1],buf,strlen(buf)+1)!=strlen(buf)+1) {
-                printf("couldnt write!\n");
                 exit(-1);
-            } else {
-                //printf("wrote %s to pipe!\n",buf);
             }
             exit(0);
         }
     }
 
+    // parent
     for(int i = 0; i < argc-1; i++) {
         int stat = 0;
         waitpid(children[i],&stat,0);
+        if(stat != 0) {
+            printf("Child %d failed with code %d\n",i,stat);
+        }
         char buf[64];
         if(read(pipes[i][0],&buf,sizeof(buf)) == 0) {
-            printf("couldnt read from pipe!\n");
+            printf("Could not read from pipe %d.\n",i);
         } else {
             printf("Fibonacci of %s is %s\n",argv[i+1],buf);
         }
