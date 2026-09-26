fcf83220e3c37a96c82f1f4e018c6890a6967901
b-atteryacid <goadam80@gmail.com>
2026-09-25 20:22:42 -0600

    build and test in minix

--- main.c
+++ main.c
@@ -36,7 +36,6 @@ uint32_t fib(uint32_t n) {
  * @param str Pointer to the string. Must be null-terminated and contain a positive integer.
  * @return The integer representation in the string.
  */
-
 uint32_t strToInt(const char* str) {
     uint32_t result = 0;
 
@@ -52,12 +51,6 @@ uint32_t strToInt(const char* str) {
     return result;
 }
 
-void debugprintargs(char** a, uint16_t im) {
-    uint16_t i = 0;
-    for(i = 0; i < im; i++) {
-        printf("/////ARG%d: %s\n",i,a[i]);
-    }
-}
 int main(int argc, char** argv) {
     int children[MAX_ARGS];
     int pipes[MAX_ARGS][2];
@@ -65,14 +58,12 @@ int main(int argc, char** argv) {
     uint32_t thisInt;
     int i;
     int stat;
-    char bufs[MAX_ARGS][STR_BUF_SIZE];
+    char buf[STR_BUF_SIZE];
 
     if(argc == 1 || argc-1 > MAX_ARGS) {
         printf("Usage: ./main (n0) [n1] [n2] ... [n%d]\n",MAX_ARGS-1);
         return -1;
     }
-
-    /*debugprintargs(argv,argc);*/
 
     for(i = 1; i < argc; i++) {
         thisArg = argv[i];
@@ -82,7 +73,7 @@ int main(int argc, char** argv) {
             printf("Could not create pipe\n");
             exit(-1);
         }
-        debugprintargs(argv,argc);
+
         children[i-1] = fork();
 
         /*error*/
@@ -93,29 +84,25 @@ int main(int argc, char** argv) {
         }
         /*child*/
         else if (children[i-1] == 0) {
-            sprintf(bufs[i-1],"%d",fib(thisInt));
-            if(write(pipes[i-1][1],bufs[i-1],strlen(bufs[i-1])+1)!=strlen(bufs[i-1])+1) {
+            sprintf(buf,"%d",fib(thisInt));
+            if(write(pipes[i-1][1],buf,strlen(buf)+1) != strlen(buf)+1) {
                 exit(-1);
             }
             exit(0);
         }
-
-        printf("arg number i %d\n",i);
     }
 
     /*parent only*/
-    printf("PARNET:\n");
     for( i = 0; i < argc-1; i++) {
-        debugprintargs(argv,argc);
         stat = 0;
         waitpid(children[i],&stat,0);
         if(stat != 0) {
             printf("Child %d failed with code %d\n",i,stat);
         }
-        if(read(pipes[i][0],&bufs[i],STR_BUF_SIZE) == 0) {
+        if(read(pipes[i][0],buf,STR_BUF_SIZE) == 0) {
             printf("Could not read from pipe %d.\n",i);
         } else {
-            printf("Fibonacci of %s is %s\n",argv[i+1],bufs[i]);
+            printf("Fibonacci of %s is %s\n",argv[i+1],buf);
         }
     }
 
